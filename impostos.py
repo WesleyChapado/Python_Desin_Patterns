3519c39b87435ced19194f440d13b68a0c5a3092
Wesley Chapado <wesley-chapado@hotmail.com>
2021-11-30 22:48:40 -0300

    melhoria

--- impostos.py
+++ impostos.py
@@ -6,9 +6,3 @@ class ICMS(object):
 class ISS(object):
     def calcula(self, orcamento):
         return orcamento.valor * 0.06
-
-# def calcula_ISS(orcamento):
-#     return orcamento.valor * 0.1
-#
-# def calcula_ICMS(orcamento):
-#     return orcamento.valor * 0.06
