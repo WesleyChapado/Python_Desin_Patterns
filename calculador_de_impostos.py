4cbf7cbf9ff1bcc5c08434a3287f22478f904f5c
Wesley Chapado <wesley-chapado@hotmail.com>
2021-12-02 17:20:26 -0300

    usando classes abstratas, Template Method

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,20 +1,28 @@
-from impostos import ICMS, ISS
-
 class Calculador_de_impostos(object):
+
     def realiza_calculo(self, orcamento, imposto):
-
-        imposto_calculado = imposto.calcula(orcamento)
-
-        print(imposto_calculado)
+        valor = imposto.calcula(orcamento)
+        print (valor)
 
 if __name__ == '__main__':
-    from orcamento import Orcamento
 
-    calculador = Calculador_de_impostos()
+    from orcamento import Orcamento, Item
 
-    orcamento = Orcamento(500)
+    # adicionado os impostos no import
+    from impostos import ISS, ICMS, ICPP, IKCV
 
-    calculador.realiza_calculo(orcamento, ISS())
-    calculador.realiza_calculo(orcamento, ICMS())
+    orcamento = Orcamento()
+    # adicionando itens ao orçamento
+    orcamento.adiciona_item(Item('ITEM 1', 50))
+    orcamento.adiciona_item(Item('ITEM 2', 200))
+    orcamento.adiciona_item(Item('ITEM 3', 250))
 
+    calculador_de_impostos = Calculador_de_impostos()
+    print ('ISS e ICMS')
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
 
+    # cálculo dos novos impostos
+    print ('ICPP e IKCV')
+    calculador_de_impostos.realiza_calculo(orcamento, ICPP()) # imprime 25.0
+    calculador_de_impostos.realiza_calculo(orcamento, IKCV()) # imprime 30.0
